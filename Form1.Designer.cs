e93d2bc636a7f2339ba73ecdafebf47cb6a601f5
A.O.Donskih <totzhe@yandex.ru>
2012-10-21 00:40:24 +0400

    After merging

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+//<< HEAD
             this.SuspendLayout();
             // 
+//===
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnTestGit = new System.Windows.Forms.Button();
+            this.tabControlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPage1);
+            this.tabControlMain.Controls.Add(this.tabPage2);
+            this.tabControlMain.Location = new System.Drawing.Point(431, 189);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(200, 100);
+            this.tabControlMain.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnTestGit
+            // 
+            this.btnTestGit.Location = new System.Drawing.Point(525, 82);
+            this.btnTestGit.Name = "btnTestGit";
+            this.btnTestGit.Size = new System.Drawing.Size(75, 23);
+            this.btnTestGit.TabIndex = 1;
+            this.btnTestGit.Text = "btnTestGit";
+            this.btnTestGit.UseVisualStyleBackColor = true;
+            // 
+//>>> origin/master
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+//<< HEAD
             this.ClientSize = new System.Drawing.Size(366, 280);
+//===
+            this.ClientSize = new System.Drawing.Size(1069, 531);
+            this.Controls.Add(this.btnTestGit);
+            this.Controls.Add(this.tabControlMain);
+//>>> origin/master
             this.Name = "FormMain";
             this.Text = "Биллинговая система";
             this.ResumeLayout(false);
@@ -43,6 +98,13 @@
 
         #endregion
 
+//<< HEAD
+//===
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnTestGit;
+//>>> origin/master
     }
 }
 
