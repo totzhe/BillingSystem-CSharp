df3d7e76cd74b78865f414a070f7a53b72297485
A.O.Donskih <totzhe@yandex.ru>
2012-11-11 22:02:36 +0400

    Revert "Not working model"
    This reverts commit 5254c404f6e230ca310bb7d7d259b5d60f2083a4.

--- DataModel/BillingDataModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\Мои документы\Study\Z\3 курс\ТП\BillingSystem-CSharp\DataModel\BillingDataModel.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
--- DatabaseUtils.cs
+++ DatabaseUtils.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
-using BillingSystem.DataModel;
 
 namespace BillingSystem
 {
-    public class DatabaseUtils
+    public static class DatabaseUtils
     {
         private static MySqlConnection _connection;
 
@@ -21,26 +20,11 @@ namespace BillingSystem
                     throw new Exceptions.NoConnectionException("Database connection not open.");
             }
         }
-        
-        private billingEntities1 context;
-
-        public DatabaseUtils()
-        {
-            context = new billingEntities1();
-        }
 
         public static void Connect(string ConnectionString)
         {
             ConnectionManager.Init(ConnectionString);
             _connection = ConnectionManager.GetConnection();
-        }
-
-        public List<subscriber> FindCustomerList(string name)
-        {
-            var q = from c in context.subscriber
-                    //where c.name.StartsWith(name)
-                    select c;
-            return q.ToList();
         }
 
 
