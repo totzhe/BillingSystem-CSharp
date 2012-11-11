5254c404f6e230ca310bb7d7d259b5d60f2083a4
A.O.Donskih <totzhe@yandex.ru>
2012-11-11 21:25:53 +0400

    Not working model

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using BillingSystem.Model;
+using BillingSystem.DataModel;
 using BillingSystem.View;
 
 namespace BillingSystem.Controllers
 {
     public class SearchController: ISearchController
     {
+        private DatabaseUtils _dbUtils;
+
+        public SearchController()
+        {
+            _dbUtils = new DatabaseUtils();
+        }
+
         public List<string[]> Search(string name, string patronymic, string surname, string phoneNumber, string debt)
         {
             //TODO: Распарсить входящие значения и преобразовать в нужны формат
@@ -17,6 +24,13 @@ namespace BillingSystem.Controllers
             List<string[]> result = new List<string[]>();
 
             //TODO: Через DatabaseUtils получить нужных абонентов, перегнать в строки, записать; если debt < 0, значит, выводятся абоненты с любым размером долга
+
+            foreach (subscriber subs in _dbUtils.FindCustomerList("a"))
+            {
+                string[] s = new string[5];
+                s[0] = subs.name;
+                result.Add(s);
+            }
 
             for (int i = 0; i < 50; i++)
             {
--- DatabaseUtils.cs
+++ DatabaseUtils.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
+using BillingSystem.DataModel;
 
 namespace BillingSystem
 {
-    public static class DatabaseUtils
+    public class DatabaseUtils
     {
         private static MySqlConnection _connection;
 
@@ -20,11 +21,26 @@ namespace BillingSystem
                     throw new Exceptions.NoConnectionException("Database connection not open.");
             }
         }
+        
+        private billingEntities1 context;
+
+        public DatabaseUtils()
+        {
+            context = new billingEntities1();
+        }
 
         public static void Connect(string ConnectionString)
         {
             ConnectionManager.Init(ConnectionString);
             _connection = ConnectionManager.GetConnection();
+        }
+
+        public List<subscriber> FindCustomerList(string name)
+        {
+            var q = from c in context.subscriber
+                    //where c.name.StartsWith(name)
+                    select c;
+            return q.ToList();
         }
 
 
